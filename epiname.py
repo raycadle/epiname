3b0ecb30272660cda808f911dad5191e01430565
N3WK1D <devel.kepada@aleeas.com>
2024-06-02 08:19:16 -0600

    major changes
    cleaned up some functions,
    seperated large functions into smaller ones,
    added some prompting functions,
    added some helper functions,
    improved some logic to prompt for info if not given,
    removed and moved some lines of code,
    some other things I may be forgetting

--- epiname.py
+++ epiname.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
 # To Do
-# feat: group seasons, if appli.
-# feat: specify naming format
-# feat: interactive when no args
+# feat: interactive when no args - done
 # feat: prompt for confirm before rename, skip with -y, --noprompt  
 # impr: store titles in cache when failure
 
@@ -18,132 +16,176 @@ import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
 
-# Parse Arguments
-parser = argparse.ArgumentParser(description='Add episode titles to the episode files of a show.')
-parser.add_argument('-e', '--edit', action='store_true', help='edit episode titles before renaming episode files')
-parser.add_argument('-n', '--nochange', action='store_true', help='perform mock-run and print')
-parser.add_argument('-v', '--verbose', action='store_true', help='show all output')
-parser.add_argument('-q', '--quiet', action='store_true', help='show no output')
-parser.add_argument('-d', '--dir', help='directory containing the media files')
-parser.add_argument('-f', '--file', help='file containing titles')
-parser.add_argument('-s', '--show', help='name of the show to search for')
-args = parser.parse_args()
-
-def get_filenames():
-  files = []
-  ext = ['.mkv', '.mp4']
-  # Get a list of files to rename
-  if args.dir:
-    if Path(args.dir).is_dir():
-      workingDir = args.dir
+def check_file(file):
+    if Path(file).is_file():
+        validFile = True
     else:
-      print('error:', args.dir, 'is not a directory')
-      sys.exit(20)
-  else:
-    workingDir = Path.cwd()
-  
-  path = Path(workingDir)
-  print('> searching for media files in', path)
-  for p in sorted(path.rglob('*')):
-    file = Path(p)
-    if file.is_file():
-      if file.suffix in ext:
-        files.append(file)
-  print('> found', len(files), 'files in', workingDir)
-  return files
+        validFile = False
 
-def get_titles():
-  titles = []
-  # Get a list of episode titles
-  if args.file:
-    if Path(args.file).is_file():
-      titleFile = args.file
+    return validFile
+
+def check_dir(directory):
+    if Path(directory).is_dir():
+        validDir = True
     else:
-      print('error:', args.file, 'is not a file')
-      sys.exit(30)
-  else:
-    titleFile = tempfile.NamedTemporaryFile().name
-    if args.show:
-      showName = args.show
-      print('searching for', showName)
-      searchQuery = "List of " + showName + " episodes"
-      searchResult = wikipedia.search(searchQuery, results=1)
-      
-      if searchResult:
+        validDir = False
+
+    return validDir
+
+def search_show(showName):
+    print(f"Searching for {showName}...")
+    searchQuery = "List of " + showName + " episodes"
+    searchResult = wikipedia.search(searchQuery, results=1)
+    titlesFile = tempfile.NamedTemporaryFile().name
+
+    if searchResult:
         showPage = wikipedia.page(searchResult[0], pageid = None, auto_suggest = False)
         response = requests.get(url=showPage.url)
         soup = BeautifulSoup(response.content, 'html.parser')
         items = soup.find(id="mw-content-text").find_all(class_="summary")
         
-        with open(titleFile, "w") as file:
-          for item in items:
-            print(item.get_text(), file=file)
+        with open(titlesFile, "w") as file:
+            for item in items:
+                print(item.get_text(), file=file)
         
-        subprocess.run(['sed', '-i', '/^List of.*episodes$/d;/^Release$/d;s/^"//g;s/".*$//g;s/:/-/g;s/?/_/g', titleFile])
-        
-      else:
-        print('error: no results were found for ', showName)
-        sys.exit(40)
+        subprocess.run(['sed', '-i', '/^List of.*episodes$/d;/^Release$/d;s/^"//g;s/".*$//g;s/:/-/g;s/?/_/g', titlesFile])
     else:
-      print('error: must provide show name if no titles file is provided')
-      sys.exit(40)
-      
-  
-  editor = os.environ.get('EDITOR', 'vi')
-  if args.edit == True:
-    subprocess.call([editor, titleFile])
+        print(f"Error: no results were found for {showName}.")
+
+    return titlesFile
+
+def rename_files(filenames, episodeTitles, dryrun):
+    # Append episode titles to the files
+    if len(filenames) == len(episodeTitles):
+        for file, title in zip(filenames, episodeTitles):
+            f = Path(file)
+            name, ext = f.stem, f.suffix
+            newName = name + ' - ' + title + ext
+            if dryrun:
+                print(f"New filename: {newName}")
+            elif not dryrun:
+                f.rename(Path(f.parent, newName))
+        print(f"All files renamed!")
+    else:
+        print(f"Error: the number of files ({len(filenames)}) and titles ({len(episodeTitles)}) do not match!")
+        sys.exit(10)
+
+def get_filenames(workingDir):
+    files = []
+    ext = ['.mkv', '.mp4']
+    print(f"Searching {workingDir} for media files...")
+    # Get a list of files to rename
+    path = Path(workingDir)
+    for p in sorted(path.rglob('*')):
+      file = Path(p)
+      if file.is_file():
+        if file.suffix in ext:
+          files.append(file)
+
+    print(f"Found {len(files)} files in {workingDir}.")
+    return files
+
+def get_titles(titlesFile, editFirst):
+    titles = []
+    # Get a list of episode titles
+    editor = os.environ.get('EDITOR', 'vi')
+    if editFirst:
+        subprocess.call([editor, titlesFile])
     
-  with open(titleFile, "r") as file:
-    for line in file:
-      titles.append(line.rstrip())
+    with open(titlesFile, "r") as file:
+        for line in file:
+            titles.append(line.rstrip())
       
-  if args.file:
-    print('> found', len(titles), 'titles in', args.file)
-  elif args.show:
-    print('> found', len(titles), 'titles for' + showName)
-  return titles
+    print(f"Found {len(titles)} titles.")
+    return titles
 
-filenames = get_filenames()
-episodeTitles = get_titles()
+def prompt_dir():
+    while True:
+        answer = input("Please enter the directory to search for media files (leave blank to search current directory): ")
+        if answer == "":
+            workingDir = Path.cwd()
+            break
+        else:
+            if check_dir(answer):
+                workingDir = answer
+                break
+            else:
+                print(f"Error: ${answer} is not a valid directory.")
+    return workingDir
 
-def rename_files():
-  # Append episode titles to the files
-  if len(filenames) == len(episodeTitles):
-    print('> renaming', len(filenames), 'files')
-    for file, title in zip(filenames, episodeTitles):
-      f = Path(file)
-      name, ext = f.stem, f.suffix
-      newName = name + ' - ' + title + ext
-      if args.verbose == True:
-        print('> renaming', f.name, 'to ' + newName + '...')
-        if args.nochange == True:
-          print('output: ' + newName)
+def prompt_titles():
+    while True:
+        answer = input("Please enter the title file's location or the show's name (leave blank to search current directory): ")
+        if answer == "":
+            workingDir = Path.cwd()
+            print(f"Searching {workingDir} for text files...")
+            path = Path(workingDir)
+            for p in sorted(path.rglob('*')):
+                file = Path(p)
+                if file.is_file():
+                    if file.suffix == ".txt":
+                        print(f"Found {file}! Scanning...")
+                        titlesSource = file
+                        break
+            break
         else:
-          f.rename(Path(f.parent, newName))
-        print('> done')
-      else:
-        if args.nochange == True:
-          print('output: ' + newName)
-        else:
-          f.rename(Path(f.parent, newName))
-    print('> all files renamed')
-  else:
-    print('error: the number of files (', len(filenames), ') and titles (', len(episodeTitles), ') do not match')
-    sys.exit(10)
+            if check_file(answer):
+                titlesSource = answer
+                break
+            else:
+                print(f"Error: {answer} is not a valid file!")
+                print(f"Searching for show using {answer} as keywords...")
+                titlesSource = search_show(answer)
+                break
+
+    return titlesSource
 
 def main():
-  #if not len(sys.argv) > 1:
-    # prompt for dir
-    #get_filenames()
-    # prompt for title file
-    #get_titles()
-    # prompt for show name
-    #get_show()
-  #else:
-    #parse_args()
-    #get_filenames()
-    #get_titles()
-    
-  rename_files()
+    # Parse Arguments
+    parser = argparse.ArgumentParser(description='Add episode titles to the episode files of a show.')
+    parser.add_argument('-e', '--edit', action='store_true', help='edit episode titles before renaming episode files')
+    parser.add_argument('-n', '--nochange', action='store_true', help='perform mock-run and print')
+    parser.add_argument('-q', '--quiet', action='store_true', help='show no output')
+    parser.add_argument('-d', '--dir', help='directory containing the media files')
+    parser.add_argument('-t', '--titles', help='source of episode titles')
+    args = parser.parse_args()
 
-main()
+    if len(sys.argv) <= 1:
+        workingDir = prompt_dir()
+        filenames = get_filenames(workingDir)
+        titlesFile = prompt_titles()
+        episodeTitles = get_titles(titlesFile, True)
+        rename_files(filenames, episodeTitles, True)
+    else:
+        if args.dir:
+            if check_dir(args.dir):
+                workingDir = args.dir
+            else:
+                print(f"Error: {args.dir} is not a directory!")
+                workingDir = prompt_dir()
+        else:
+            workingDir = prompt_dir()
+
+        filenames = get_filenames(workingDir)
+        
+        if args.titles:
+            if check_file(args.titles):
+                titlesFile = args.titles
+            else:
+                print(f"Error: {args.titles} is not a directory!")
+                titlesFile = prompt_titles()
+        else:
+            titlesFile = prompt_titles()
+
+        if args.edit:
+            episodeTitles = get_titles(titlesFile, True)
+        else:
+            episodeTitles = get_titles(titlesFile, False)
+
+        if args.nochange:
+            rename_files(filenames, episodeTitles, True)
+        else:
+            rename_files(filenames, episodeTitles, False)
+
+if __name__ == '__main__':
+    main()
